95190c861fc90098f522a0cb159b40837b43dfad
Jan Peleska <jp@informatik.uni-bremen.de>
2017-04-18 04:50:37 +0200

    Add a standard test generator main program in new directory generator

---
+++ src/generator/fsm-test-generator.cpp
@@ -0,0 +1,236 @@
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <stdlib.h>
+#include <string.h>
+
+#include <interface/FsmPresentationLayer.h>
+#include <fsm/Dfsm.h>
+#include <fsm/Fsm.h>
+#include <fsm/FsmNode.h>
+#include <fsm/IOTrace.h>
+#include <fsm/FsmPrintVisitor.h>
+#include <fsm/FsmSimVisitor.h>
+#include <fsm/FsmOraVisitor.h>
+#include <trees/IOListContainer.h>
+#include <trees/OutputTree.h>
+#include <trees/TestSuite.h>
+
+
+using namespace std;
+
+
+/**
+ *   Program execution parameters and associated types
+ */
+typedef enum {
+    FSM_CSV,
+    FSM_JASON,
+    FSM_BASIC
+} model_type_t;
+
+typedef enum {
+    WMETHOD,
+    WPMETHOD
+} generation_method_t;
+
+static model_type_t modelType;
+static string modelFile;
+static string plStateFile;
+static string plInputFile;
+static string plOutputFile;
+static string fsmName;
+static string testSuiteFileName;
+static generation_method_t genMethod;
+static unsigned int numAddStates;
+
+static shared_ptr<FsmPresentationLayer> pl = nullptr;
+static shared_ptr<Dfsm> dfsm = nullptr;
+static shared_ptr<Fsm> fsm = nullptr;
+
+static bool isDeterministic = false;
+
+
+/**
+ * Write program usage to standard error.
+ * @param name program name as specified in argv[0]
+ */
+static void printUsage(char* name) {
+    cerr << "usage: " << name << " [-w] [-n fsmname] [-p infile outfile statefile] [-a additionalstates] [-t testsuitename] modelfile" << endl;
+}
+
+/**
+ * Parse parameters, stop execution if parameters are illegal.
+ *
+ * @param argc parameter 1 from main() invocation
+ * @param argv parameter 2 from main() invocation
+ */
+static void parseParameters(int argc, char* argv[]) {
+    
+    // Set default parameters
+    genMethod = WPMETHOD;
+    fsmName = string("FSM");
+    testSuiteFileName = string("testsuite.txt");
+    numAddStates = 0;
+    
+    for ( int p = 1; p < argc; p++ ) {
+        if ( strcmp(argv[p],"-w") == 0 ) {
+            genMethod = WMETHOD;
+        }
+        else if ( strcmp(argv[p],"-n") == 0 ) {
+            if ( argc < p+2 ) {
+                cerr << argv[0] << ": missing FSM name" << endl;
+                printUsage(argv[0]);
+                exit(1);
+            }
+            else {
+                fsmName = string(argv[++p]);
+            }
+        }
+        else if ( strcmp(argv[p],"-t") == 0 ) {
+            if ( argc < p+2 ) {
+                cerr << argv[0] << ": missing test suite name" << endl;
+                printUsage(argv[0]);
+                exit(1);
+            }
+            else {
+                testSuiteFileName = string(argv[++p]);
+            }
+        }
+        else if ( strcmp(argv[p],"-a") == 0 ) {
+            if ( argc < p+2 ) {
+                cerr << argv[0] << ": missing number of additional states" << endl;
+                printUsage(argv[0]);
+                exit(1);
+            }
+            else {
+                numAddStates = atoi(argv[++p]);
+            }
+        }
+        else if ( strcmp(argv[p],"-p") == 0 ) {
+            if ( argc < p+4 ) {
+                cerr << argv[0] << ": missing presentation layer files" << endl;
+                printUsage(argv[0]);
+                exit(1);
+            }
+            else {
+                plInputFile = string(argv[++p]);
+                plOutputFile = string(argv[++p]);
+                plStateFile = string(argv[++p]);
+           }
+        }
+        else if ( strstr(argv[p],".csv")  ) {
+            modelFile = string(argv[p]);
+            modelType = FSM_CSV;
+        }
+        else if ( strstr(argv[p],".fsm")  ) {
+            modelFile = string(argv[p]);
+            modelType = FSM_BASIC;
+        }
+        else if ( strstr(argv[p],".@todo")  ) {
+            modelFile = string(argv[p]);
+            modelType = FSM_JASON;
+        }
+        else {
+            cerr << argv[0] << ": illegal parameter `" << argv[p] << "'" << endl;
+            printUsage(argv[0]);
+            exit(1);
+        }
+    }
+    
+    if ( modelFile.empty() ) {
+        cerr << argv[0] << ": missing model file" << endl;
+        printUsage(argv[0]);
+        exit(1);
+    }
+    
+}
+
+
+static void readModel() {
+    
+    switch ( modelType ) {
+        case FSM_CSV:
+            isDeterministic = true;
+            dfsm = make_shared<Dfsm>(modelFile,fsmName);
+            pl = dfsm->getPresentationLayer();
+            break;
+            
+        case FSM_JASON:
+            // @todo
+            break;
+            
+        case FSM_BASIC:
+            if ( plStateFile.empty() ) {
+                pl = make_shared<FsmPresentationLayer>();
+            }
+            else {
+                pl = make_shared<FsmPresentationLayer>(plInputFile,plOutputFile,plStateFile);
+            }
+            fsm = make_shared<Fsm>(modelFile,pl,fsmName);
+            if ( fsm->isDeterministic() ) {
+                dfsm = make_shared<Dfsm>(modelFile,pl,fsmName);
+                fsm = nullptr;
+            }
+            break;
+    }
+    
+}
+
+static void generateTestSuite() {
+    
+    shared_ptr<TestSuite> testSuite =
+    make_shared<TestSuite>();
+    
+    switch ( genMethod ) {
+        case WMETHOD:
+            if ( dfsm != nullptr ) {
+                IOListContainer iolc = dfsm->wMethod(numAddStates);
+                for ( auto inVec : *iolc.getIOLists() ) {
+                    shared_ptr<InputTrace> itrc = make_shared<InputTrace>(inVec,pl);
+                    testSuite->push_back(dfsm->apply(*itrc));
+                }
+            }
+            else {
+                IOListContainer iolc = fsm->wMethod(numAddStates);
+                for ( auto inVec : *iolc.getIOLists() ) {
+                    shared_ptr<InputTrace> itrc = make_shared<InputTrace>(inVec,pl);
+                    testSuite->push_back(fsm->apply(*itrc));
+                }
+            }
+            break;
+        case WPMETHOD:
+            if ( dfsm != nullptr ) {
+                IOListContainer iolc = dfsm->wpMethod(numAddStates);
+                for ( auto inVec : *iolc.getIOLists() ) {
+                    shared_ptr<InputTrace> itrc = make_shared<InputTrace>(inVec,pl);
+                    testSuite->push_back(dfsm->apply(*itrc));
+                }
+            }
+            else {
+                IOListContainer iolc = fsm->wpMethod(numAddStates);
+                for ( auto inVec : *iolc.getIOLists() ) {
+                    shared_ptr<InputTrace> itrc = make_shared<InputTrace>(inVec,pl);
+                    testSuite->push_back(fsm->apply(*itrc));
+                }
+            }
+            break;
+    }
+    
+    testSuite->save(testSuiteFileName);
+    
+}
+
+int main(int argc, char* argv[])
+{
+    
+    parseParameters(argc,argv);
+    readModel();
+    generateTestSuite();
+    
+    exit(0);
+    
+}
+
+
+
