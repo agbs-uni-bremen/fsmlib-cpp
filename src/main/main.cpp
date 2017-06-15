0a3c803ab5572ebcd0c80fe3c24fc5dc3a63313c
Jan Peleska <jp@informatik.uni-bremen.de>
2017-06-15 22:17:25 +0200

    Update of test suite

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -738,6 +738,15 @@ int main()
 
 #endif
     
+    
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+    test6();
+    test7();
+    test8();
     test9();
     test10();
     
