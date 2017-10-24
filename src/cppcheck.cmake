fcf2a136c0878291a57d89eba2bc6fb7af19ca8a
andzno1 <aromero@informatik.uni-bremen.de>
2017-10-24 13:24:04 +0200

    Added cmake target for cppcheck.

---
+++ src/cppcheck.cmake
@@ -0,0 +1,31 @@
+# additional target to perform cppcheck run, requires cppcheck
+
+# get all project files
+# HACK this workaround is required to avoid qml files checking ^_^
+file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
+file(GLOB_RECURSE EXCLUDE
+    "${CMAKE_SOURCE_DIR}/externals/jsoncpp-0.10.0/*"
+    "${CMAKE_SOURCE_DIR}/externals/easyloggingpp_v9.95.0/logging/easylogging++.cc"
+    "${CMAKE_SOURCE_DIR}/externals/easyloggingpp_v9.95.0/logging/easylogging++.h")
+list(REMOVE_ITEM ALL_SOURCE_FILES ${EXCLUDE})
+message("${ALL_SOURCE_FILES}")
+#foreach (SOURCE_FILE ${ALL_SOURCE_FILES})
+#    string(FIND ${SOURCE_FILE} ${PROJECT_TRDPARTY_DIR} PROJECT_TRDPARTY_DIR_FOUND)
+#    if (NOT ${PROJECT_TRDPARTY_DIR_FOUND} EQUAL -1)
+#        list(REMOVE_ITEM ALL_SOURCE_FILES ${SOURCE_FILE})
+#    endif ()
+#endforeach ()
+
+add_custom_target(
+        cppcheck
+        COMMAND /usr/bin/cppcheck
+        -I ${CMAKE_SOURCE_DIR}
+#        --check-config
+        --enable=warning,performance,portability,information,missingInclude
+        --std=c++11
+        --library=qt.cfg
+        --template="[{severity}][{id}] {message} {callstack} \(On {file}:{line}\)"
+        --verbose
+        --quiet
+        ${ALL_SOURCE_FILES}
+)
