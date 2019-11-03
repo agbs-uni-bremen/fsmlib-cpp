e806fd9e5170f35cad290e8edfee6091316b0ba7
Jan Peleska <peleska@uni-bremen.de>
2019-11-03 13:37:30 +0100

    Remove superfluous function getcwd().

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -2212,17 +2212,6 @@ void adaptiveTestRandom(AdaptiveTestConfig& config)
     printSummary(config.testName, executed, passed, i - executed, durationS, durationM);
 }
 
-std::string getcwd() {
-    std::string result(1024,'\0');
-    while( getcwd(&result[0], result.size()) == 0) {
-        if( errno != ERANGE ) {
-          throw std::runtime_error(strerror(errno));
-        }
-        result.resize(result.size()*2);
-    }
-    result.resize(result.find('\0'));
-    return result;
-}
 
 void trial(bool debug)
 {
